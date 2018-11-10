a96cfc0a82e3ca02c7c3706519802b9d63641fe7
Justin Singh <justin.singh125125@gmail.com>
2018-11-10 12:06:16 -0800

    fixed schema

--- scheme.sql
+++ scheme.sql
@@ -2,14 +2,17 @@ DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 USE bamazon;
 
-CREATE TABLE products(
-    item_id INTEGER(10)UNIQUE NULL,
+CREATE TABLE products
+(
+    item_id INTEGER(10) NOT NULL,
     product_name VARCHAR(40) NULL,
     department_name VARCHAR(40) NULL,
-    price DECIMAL(10,2)NULL,
+    price DECIMAL(10 , 2)NULL,
     stock_quantity INTEGER(10) NULL,
-    primary key(item_id)
-)
-SELECT * FROM products;
+    PRIMARY KEY (item_id)
+);
+
+SELECT *
+FROM products;
 
 
